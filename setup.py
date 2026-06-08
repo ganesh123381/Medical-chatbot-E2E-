76ef3ee751bccd8b8128f57664b2ac286e22f021
ganesh123381 <ganeshvhitman@gmail.com>
2026-06-08 23:49:02 +0200

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Ganesh V",
+    author_email="ganeshvhitman@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
